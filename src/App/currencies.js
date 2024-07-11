3369cc62ceb616d57d3fa3bcc0490dc73fd70d28
Adam Gralak <gralak.adam2@wp.pl>
2024-07-11 11:16:31 +0200

    added currencies

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.45,
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 3.98,
+    },
+    {
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 3.90,
+    },
+]
