d3897da1b99414be5180960787f9a053e8287a20
Adam Gralak <gralak.adam2@wp.pl>
2024-07-13 21:04:50 +0200

    changed in form

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,12 @@
-import { useState } from 'react';
-import { currencies } from './currencies';
 import { Form } from './Form';
 import './App.css';
 import React from 'react';
 
 function App() {
 
-  const [result, setResult] = useState();
-
-  const calculateResult = (amount, currency) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-
-  }
-
   return (
     <div className="app">
-      <Form
-        result={result}
-        calculateResult={calculateResult}
-      />
+      <Form />
     </div>
   );
 }
--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -4,9 +4,24 @@ import { Result } from "./Result";
 import React from "react";
 import './form.css'
 
-export const Form = ({ result, calculateResult }) => {
+export const Form = () => {
+    const [result, setResult] = useState();
+
+    const calculateResult = (amount, currency) => {
+      const rate = currencies
+        .find(({ short }) => short === currency)
+        .rate;
+  
+      setResult({
+        sourceAmount: +amount,
+        targetAmount: amount / rate,
+        currency,
+      });
+  
+    }
+
     const [currency, setCurrency] = useState(currencies[0].short);
-    const [ amount, setAmount ] = useState("");
+    const [amount, setAmount] = useState("");
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -14,48 +29,50 @@ export const Form = ({ result, calculateResult }) => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <>
             <h1 className="form_header">Przelicznik walut </h1>
-            <p className="form_label">
-                <label>
-                    <span> Kwota w zł:</span>
-                    <input
-                        className="form_input"
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        placeholder="Wpsiz kwotę"
-                        required
-                        step={0.01}
-                        type="number"
-                    />
-                </label>
-            </p>
-            <p className="form_label">
-                <label>
-                    <span>Waluta:</span>
-                    <select
-                        className="form_input"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        )))}
+            <form onSubmit={onSubmit}>
+                <p className="form_label">
+                    <label>
+                        <span> Kwota w zł:</span>
+                        <input
+                            className="form_input"
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            placeholder="Wpsiz kwotę"
+                            required
+                            step={0.01}
+                            type="number"
+                        />
+                    </label>
+                </p>
+                <p className="form_label">
+                    <label>
+                        <span>Waluta:</span>
+                        <select
+                            className="form_input"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
 
-                    </select>
-                </label>
-            </p>
-            <p>
-                <button className="form_button">Przelicz</button>
-            </p>
-            <p className="form_info">Kurs walut na dzień: 11 lipca 2024r.</p>
-            <Result result={result} />
-        </form>
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <button className="form_button">Przelicz</button>
+                </p>
+                <p className="form_info">Kurs walut na dzień: 11 lipca 2024r.</p>
+                <Result result={result} />
+            </form>
+        </>
     )
 
 
